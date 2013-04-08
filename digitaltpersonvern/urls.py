15ed03beda6278bcc5226e82ce04c19f3bfaaa6a
Sigurd Gartmann <sigurdga@sigurdga.no>
2013-04-08 15:37:52 +0200

    Upgrade to django 1.5, with dependencies

--- digitaltpersonvern/urls.py
+++ digitaltpersonvern/urls.py
@@ -1,11 +1,9 @@
 from django.conf.urls.defaults import patterns, include, url
-from django.views.generic.simple import direct_to_template
 
 from django.contrib import admin
 from django.conf import settings
 admin.autodiscover()
 
-from views import IndexView
 from feeds import LatestEntriesFeed
 
 from samklang_pages.views import page
