96763d2979279f3fa3253551953f21587dc1445c
Sigurd Gartmann <sigurdga@sigurdga.no>
2016-06-27 18:52:01 +0200

    Removing more unused

--- mkadmin.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-from wsgi import *
-from django.contrib.auth.models import User
-u, created = User.objects.get_or_create(username='sigurdga')
-if created:
-    u.set_password('secret')
-    u.is_superuser = True
-    u.is_staff = True
-    u.save()
